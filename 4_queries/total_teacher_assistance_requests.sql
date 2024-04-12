915a7bd8f759122a67a9f14bd84804ebe7523073
Trevor Sullivan <trevorjohnsullivan@gmail.com>
2024-04-12 17:36:02 -0230

    update

--- 4_queries/total_teacher_assistance_requests.sql
+++ 4_queries/total_teacher_assistance_requests.sql
@@ -3,3 +3,5 @@ FROM assistance_requests
 JOIN teachers ON teachers.id = teacher_id
 WHERE name = 'Waylon Boehm'
 GROUP BY teachers.name;
+
+
---
+++ student_cohorts.sql
@@ -0,0 +1,6 @@
+CREATE TABLE test ( 
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  start_date DATE,
+  end_date DATE
+);
